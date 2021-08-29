524f7a5210e1fe9dd2ecfd27899c9d91e7eea30a
cusoq <cusoq@mail.ru>
2021-08-30 01:12:47 +0300

    Add modal menu, add no-js menu, set modal-menu script

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,22 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var headerBurger = document.querySelector('.header__burger');
+var headerNavigation = document.querySelector('.navigation');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+var addBurger = function () {
+  headerBurger.classList.add('header__burger--js');
+  headerNavigation.classList.add('navigation--js');
+};
+
+addBurger();
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
