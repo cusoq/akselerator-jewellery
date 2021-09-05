c2bef6829db7c9f5a3dedccfe313594152c1a0f7
cusoq <cusoq@mail.ru>
2021-09-05 15:50:59 +0300

    Add the catalog page, set the catalog page slider, add the catalog page layout, styles

--- source/js/script.js
+++ source/js/script.js
@@ -2,16 +2,20 @@
 var headerBurger = document.querySelector('.header__burger');
 var headerNavigation = document.querySelector('.navigation');
 var headerSearch = document.querySelector('.search');
+var filterButton = document.querySelector('.main__filter-button');
+// var filterBlock = document.querySelector('.main__filter-block');
 // var headerToggle = document.querySelector('.page-header__toggle');
 
 
-var addBurger = function () {
+var isJsBlock = function () {
   headerBurger.classList.add('header__burger--js');
   headerNavigation.classList.add('navigation--js');
   headerSearch.classList.add('search--js');
+  filterButton.classList.add('main__filter-button--js');
+  // filterBlock.classList.add('main__filter-block--js');
 };
 
-addBurger();
+isJsBlock();
 
 // headerToggle.addEventListener('click', function () {
 //   if (pageHeader.classList.contains('page-header--closed')) {
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -5008,6 +5008,39 @@ const myModal2 = new HystModal({
   initSlider();
 })();
 
+(function () {
+  let swiperCatalog;
+  let sliderContainer = document.querySelector('.catalog__slider');
+  let sliderNavigation = document.querySelector('.catalog__slider-navigation');
+
+  function initCatalogSwiper() {
+    swiperCatalog = new window.Swiper('.catalog__slider', {
+      loop: true,
+      slidesPerView: 1,
+      pagination: {
+        el: document.querySelector('.catalog-slider-navigation__pagination'),
+        clickable: 'true',
+        renderBullet(index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+      },
+
+      navigation: {
+        nextEl: '.catalog-slider-navigation__btn--next',
+        prevEl: '.catalog-slider-navigation__btn--prev',
+      },
+    });
+  }
+
+  function initCatalogSlider() {
+    if (sliderContainer && sliderNavigation) {
+      initCatalogSwiper();
+    }
+  }
+
+  initCatalogSlider();
+})();
+
 //////////////////////////////////////////////////////////////////////////////////////
 (function () {
   let accordionBlock = document.querySelector('.faq__list');
